f5481875ce816f69b1e9ad74736355b9ea8107fe
muhammad6153 <owais6153@gmail.com>
2023-12-07 05:35:58 +0500

    Basic Header Setup

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,80 @@
-const Header = () => {
-  return <>Header</>;
-};
-export default Header;
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [
+    "About Us",
+    "What we do",
+    "Why choose us",
+    "Testimonial",
+    "Contact",
+  ];
+
+  return (
+    <Navbar
+      onMenuOpenChange={setIsMenuOpen}
+      maxWidth={"xl"}
+      shouldHideOnScroll={true}
+      classNames="justify-center"
+    >
+      <NavbarContent data-justify={""}>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="lg:hidden"
+        />
+        <NavbarBrand>
+          <Image
+            src="/images/logo.png"
+            height={56}
+            width={162}
+            alt="Future Humans Logo"
+          />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden lg:flex gap-[60px] ml-[60px]">
+        {menuItems.map((item, index) => (
+          <NavbarItem key={index}>
+            <Link href="#" className="text-[16px] text-dark">
+              {item}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end" className="gap-[60px] bl-1">
+        <NavbarItem className="hidden lg:flex ">
+          <Link className="text-secondary text-[16px]" href="#">
+            Login
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-primary text-[16px]" href="#">
+            Sign Up
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link className="w-full text-dark" href="#" size="lg">
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import Header from "@/components/Header";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { League_Spartan } from "next/font/google";
+const leagueSpartan = League_Spartan({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Header />;
+  return (
+    <main className={leagueSpartan.className}>
+      <div className="gradient-primary text-center py-[12px] text-[12px]">
+        <p>
+          We&lsquo;re assembling a United Tech Force to help non-profits and
+          startups affected by the war
+        </p>
+      </div>
+      <Header />;
+    </main>
+  );
 }
