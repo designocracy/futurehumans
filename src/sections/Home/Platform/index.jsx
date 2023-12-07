d6382c5c62e154a75e77b9fdfc549f3846b09ea6
muhammad6153 <owais6153@gmail.com>
2023-12-07 22:26:31 +0500

    Changes

--- src/sections/Home/Platform/index.jsx
+++ src/sections/Home/Platform/index.jsx
@@ -1,7 +1,14 @@
 import Container from '@/interfaces/Container'
 import Flex from '@/interfaces/Flex'
+import classNames from 'classnames'
 
 export default function HomePlatform() {
+  const statsClasses = classNames(
+    'text-[120px] mt-[20px] -tracking-[3.6px] leading-[92.46%] font-black gradient-primary-text',
+  )
+  const statsTextClasses = classNames(
+    'border-b-2 pb-[10px] mt-[20px] border-secondary text-black -tracking-[0.9px] leading-[92%] text-[30px]  font-light',
+  )
   return (
     <Container className={'mt-[200px] pb-[200px]'}>
       <h3 className="text-black text-[20px] leading-[92%] font-normal">PLATFORM FUTURE HUMAN</h3>
@@ -16,38 +23,22 @@ export default function HomePlatform() {
           </p>
         </div>
         <div className="">
-          <h1 className="text-[120px] mt-[20px] -tracking-[3.6px] leading-[92.46%] font-black gradient-primary-text">
-            $110,000,000+
-          </h1>
-          <p className="border-b-2 pb-[10px] mt-[20px] border-secondary text-black -tracking-[0.9px] leading-[92%] text-[30px]  font-light">
-            Total value of projects
-          </p>
+          <h1 className={statsClasses}>$110,000,000+</h1>
+          <p className={statsTextClasses}>Total value of projects</p>
         </div>
       </Flex>
       <Flex className={'mt-[50px]'} justify={'between'}>
-        <div className="max-w-[50%]">
-          <h1 className="text-[120px] mt-[20px] -tracking-[3.6px] leading-[92.46%] font-black gradient-primary-text">
-            2,000+
-          </h1>
-          <p className="border-b-2 pb-[10px] mt-[20px] border-secondary text-black -tracking-[0.9px] leading-[92%] text-[30px]  font-light">
-            Projects to date
-          </p>
+        <div className="">
+          <h1 className={statsClasses}>2,000+</h1>
+          <p className={statsTextClasses}>Projects to date</p>
         </div>
         <div className="">
-          <h1 className="text-[120px] mt-[20px] -tracking-[3.6px] leading-[92.46%] font-black gradient-primary-text">
-            100,000+
-          </h1>
-          <p className="border-b-2 pb-[10px] mt-[20px] border-secondary text-black -tracking-[0.9px] leading-[92%] text-[30px]  font-light">
-            Dediacted developers
-          </p>
+          <h1 className={statsClasses}>100,000+</h1>
+          <p className={statsTextClasses}>Dediacted developers</p>
         </div>
         <div className="">
-          <h1 className="text-[120px] mt-[20px] -tracking-[3.6px] leading-[92.46%] font-black gradient-primary-text">
-            2,000+
-          </h1>
-          <p className="border-b-2 pb-[10px] mt-[20px] border-secondary text-black -tracking-[0.9px] leading-[92%] text-[30px]  font-light">
-            Approved service provider
-          </p>
+          <h1 className={statsClasses}>2,000+</h1>
+          <p className={statsTextClasses}>Approved service provider</p>
         </div>
       </Flex>
     </Container>
